f4201504b2b439106b7062a5a4ce9cc2459c87e8
Gen639 <evgeny.goncharov.pro@gmail.com>
2023-11-04 13:56:34 +0100

    first commit

---
+++ Movie API/index.js
@@ -0,0 +1,64 @@
+const apiKey = "00368a5cf23e12b81e9b5bfd42a171f5";
+const moviesContainer = document.querySelector("#moviesContainer");
+const formSearch = document.getElementById("form");
+const searchInput = document.getElementById("searchId");
+
+let genreList = {};
+
+const getGenre = (genre_ids) => {
+  const genreNames = genre_ids.map((genre_id) => {
+    const genre = genreList.find((item) => item.id === genre_id);
+    return genre ? genre.name : "unknown genre";
+  });
+  return genreNames.join(", ");
+};
+
+const showMovies = (movies) => {
+  moviesContainer.innerHTML = "";
+
+  movies.forEach(({ poster_path, original_title, overview, genre_ids }) => {
+    const limitedOverview =
+      overview.length > 100 ? overview.slice(0, 100) + "..." : overview;
+
+    moviesContainer.innerHTML += `
+    <div class="card col-lg-3 col-xs-12 col-md-6">
+      <img src="https://image.tmdb.org/t/p/original${poster_path}" alt="${original_title}">
+              <div class="card-body">
+              <h3 class="card-header">${original_title}</h3>
+              <h5 class="card-title">${limitedOverview}</h5>
+              <p class="card-text">${getGenre(genre_ids)}</p>
+              
+          </div>
+      </div>
+    `;
+  });
+};
+
+const searchMovies = async (e) => {
+  e.preventDefault();
+  try {
+    const search = searchInput.value;
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${apiKey}`
+    );
+
+    const movies = res.data.results;
+    showMovies(movies);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const allGenreObj = async () => {
+  try {
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${apiKey}`
+    );
+    genreList = res.data.genres;
+    console.log(genreList);
+  } catch (error) {
+    console.error(error);
+  }
+};
+allGenreObj();
+formSearch.addEventListener("submit", searchMovies);
